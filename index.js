7c8bd12beb82d232af6e675338dd42840e5d6795
beyu099 <firstadebayo@gmail.com>
2023-08-11 11:00:00 +0100

    Excercise Solved

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const {largeNumber, cut3} = require('./util/utilOne')
+const utilTwo = require('./util/utilTwo')
 
+let array = [120, 1000, 9000, 25000]
+let arrayOne = [240, 2000, 18000, 50000]
+
+console.log(largeNumber(array))
+console.log(cut3(array))
+console.log(utilTwo.sum(array))
+console.log(utilTwo.concat(array, arrayOne))
---
+++ util/utilTwo.js
@@ -0,0 +1,20 @@
+
+// add numbers in an array
+const sum = (arr) => {
+    const total = 0;
+    arr.forEach((x) => {
+      total += x;
+    })
+    return total
+  }
+  
+  // concatenate two arrays
+  const concat = (arr1, arr2) => {
+    return [...arr1, ...arr2]
+  }
+  
+   // exporting the functions
+   module.exports = {
+    sum,
+    concat,
+  }
